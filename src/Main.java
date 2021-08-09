db4c644e93ce1e56968ec2c6812fd28bd82c41a9
Leandro <lcaetano30@gmail.com>
2021-08-09 17:55:51 -0300

    aplicação 1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            String product1 = "Computer";
+            String product2 = "Office desk";
+
+            int age = 38;
+            int code = 5290;
+
+            char gender = 'F';
+
+            double price1 = 2100.0;
+            double price2 = 650.50;
+            double measure = 53.12345;
+
+            System.out.println("Producta:");
+            System.out.printf("%s, which price is R$ %.2f %n", product1, price1);
+            System.out.printf("%s, which price is R$ %.2f %n", product2, price2);
+            System.out.println("Testando código");
+        }
+    }
+
+
